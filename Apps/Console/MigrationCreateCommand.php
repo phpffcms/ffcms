1cef693eb1b2941fa77547efe7f8edb649923c67
zenn <zenn1989@gmail.com>
2017-01-14 13:29:56 +0300

    Create migrations in custom directory

--- Apps/Console/MigrationCreateCommand.php
+++ Apps/Console/MigrationCreateCommand.php
@@ -5,9 +5,11 @@ namespace Apps\Console;
 
 use Ffcms\Console\Command;
 use Ffcms\Core\Helper\FileSystem\File;
+use Ffcms\Core\Helper\Type\Obj;
 use Ffcms\Core\Helper\Type\Str;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -23,7 +25,8 @@ class MigrationCreateCommand extends Command
     {
         $this->setName('migration:create')
             ->setDescription('Create new migration for cms database')
-            ->addArgument('name', InputArgument::REQUIRED, 'Set name of new migration. Example: create_demo_table');
+            ->addArgument('name', InputArgument::REQUIRED, 'Set name of new migration. Example: create_demo_table')
+            ->addOption('dir', 'dir', InputOption::VALUE_OPTIONAL, 'Set output directory for new migration file. Example: /vendor/myname/package/migrations');
     }
 
     /**
@@ -37,6 +40,14 @@ class MigrationCreateCommand extends Command
         // get migration name from params
         $name = $input->getArgument('name');
         $name = Str::lowerCase($name);
+        // get output directory
+        $dir = $this->option('dir');
+        if ($dir === null || Str::likeEmpty($dir)) {
+            $dir = '/Private/Migrations/';
+        } else {
+            $dir = rtrim($dir, '\\/');
+            $dir .= '/';
+        }
         // parse table name
         list ($action, $table, $etc) = explode('_', $name);
         if ($table === null || Str::likeEmpty($table)) {
@@ -47,7 +58,7 @@ class MigrationCreateCommand extends Command
         // work with migration template: read & parse & save
         $tpl = File::read('/Private/Carcase/Migration.tphp');
         $classContent = Str::replace(['%class%', '%table%'], [$name, $table], $tpl);
-        $fullPath = '/Private/Migrations/' . $name . '-' . $suffix . '.php';
+        $fullPath = $dir . $name . '-' . $suffix . '.php';
         File::write($fullPath, $classContent);
         // show success msg
         $output->write('New migration is created: ' . $fullPath);
