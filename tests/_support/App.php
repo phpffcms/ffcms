c8a870bf215e11f2d5abe29608ad89a58fc141a7
zenn <zenn1989@gmail.com>
2019-01-11 10:31:05 +0300

    F*ck this bullsh@t of codeception, have no idea why class mocking not work anymore. Remove 1-button test run.

--- tests/_support/App.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// load App mock fake from core tests
-require realpath(__DIR__ . '/../../') .  '/vendor/phpffcms/ffcms-core/tests/_support/App.php';
--- tests/unit/_bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Here you can initialize variables that will be available to your tests
-$file = 'vendor/Autoload.php';
-if (file_exists($file)) {
-    require ($file);
-}
